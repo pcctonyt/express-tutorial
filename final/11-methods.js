b1d2c0de1e14ec25597858ba5ce8adcf60d0093b
pcctonyt <104700781+pcctonyt@users.noreply.github.com>
2022-08-26 12:50:20 -0500

    8/26/2022 exp tut 7:56 of 8:16 done

--- final/11-methods.js
+++ final/11-methods.js
@@ -1,14 +1,18 @@
+//app.js gets really long with even a few routes, so we'll need the router, which is next
 const express = require('express')
 const app = express()
 let { people } = require('./data')
 
 // static assets
-app.use(exapress.static('./methods-public'))
-// parse form data
+app.use(express.static('./methods-public'))
+// parse form data, the extended key keeps us from having to install the body parser; the middleware is built in now, when it's false, it allows parsing of the URL encoded data with the querystring library
 app.use(express.urlencoded({ extended: false }))
+//the URLs being the same doesn't change the fact that the methods are different, the get reads data, the post adds data
+
+
 // parse json
 app.use(express.json())
-
+// get with :id at the end of the path is for a single object, the paths can be the same even if the methods are different
 app.get('/api/people', (req, res) => {
   res.status(200).json({ success: true, data: people })
 })
@@ -20,7 +24,18 @@ app.post('/api/people', (req, res) => {
       .status(400)
       .json({ success: false, msg: 'please provide name value' })
   }
+  //make sure to use the json instead even though we can get away with not using it in this example
   res.status(201).json({ success: true, person: name })
+})
+
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' })
+  }
+  res.status(201).json({ success: true, data: [...people, name] })
 })
 
 app.post('/api/postman/people', (req, res) => {
@@ -41,11 +56,11 @@ app.post('/login', (req, res) => {
 
   res.status(401).send('Please Provide Credentials')
 })
-
+// update data
 app.put('/api/people/:id', (req, res) => {
   const { id } = req.params
   const { name } = req.body
-
+ 
   const person = people.find((person) => person.id === Number(id))
 
   if (!person) {
